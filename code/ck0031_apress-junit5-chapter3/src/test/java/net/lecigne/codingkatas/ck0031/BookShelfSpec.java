680df34252e52e3626eba64f278f09b60584ed69
Anthony Le Cigne <dev@lecigne.net>
2021-02-21 12:57:56 +0100

    [CK0031] Refactor: use a Book class

--- code/ck0031_apress-junit5-chapter3/src/test/java/net/lecigne/codingkatas/ck0031/BookShelfSpec.java
+++ code/ck0031_apress-junit5-chapter3/src/test/java/net/lecigne/codingkatas/ck0031/BookShelfSpec.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,39 +17,57 @@ import static org.junit.jupiter.api.Assertions.fail;
 class BookShelfSpec {
 
     private BookShelf shelf;
+    private Book effectiveJava;
+    private Book codeComplete;
+    private Book mythicalManMonth;
 
     @BeforeEach
     void setUp() {
         shelf = new BookShelf();
+        effectiveJava = new Book.Builder()
+                .title("Effective Java")
+                .author("Joshua Bloch")
+                .publishedOn(LocalDate.of(2008, Month.MAY, 8))
+                .build();
+        codeComplete = new Book.Builder()
+                .title("Code Complete")
+                .author("Steve McConnel")
+                .publishedOn(LocalDate.of(2004, Month.JUNE, 9))
+                .build();
+        mythicalManMonth = new Book.Builder()
+                .title("The Mythical Man-Month")
+                .author("Frederick Phillips Brooks")
+                .publishedOn(LocalDate.of(1975, Month.JANUARY, 1))
+                .build();
     }
 
     @Test
     @DisplayName("is empty when no book is added to it")
     void shelfEmptyWhenNoBookAdded() {
-        List<String> books = shelf.books();
+        List<Book> books = shelf.books();
         assertTrue(books.isEmpty(), "BookShelf should be empty.");
     }
 
     @Test
     void shelfContainsTwoBooksWhenTwoBooksAdded() {
-        shelf.add("Effective Java", "Code Complete");
-        List<String> books = shelf.books();
+        shelf.add(effectiveJava, codeComplete);
+        List<Book> books = shelf.books();
         assertEquals(2, books.size(), "Bookshelf should have two books.");
     }
 
     @Test
     void shelfEmptyWhenEmptyContentIsAdded() {
         shelf.add();
-        List<String> books = shelf.books();
+        List<Book> books = shelf.books();
         assertTrue(books.isEmpty(), "Bookshelf should be empty when adding empty content");
     }
 
     @Test
     void collectionReturnedFromShelfIsImmutableForClient() {
-        shelf.add("Effective Java", "Code Complete");
-        List<String> books = shelf.books();
+        shelf.add(effectiveJava, codeComplete);
+        List<Book> books = shelf.books();
         try {
-            books.add("The Mythical Man-Month");
+            books.add(mythicalManMonth);
             fail("Book collection should be immutable for the client.");
         } catch (Exception e) {
             assertTrue(e instanceof UnsupportedOperationException, "Bookshelf should throw " +
@@ -57,18 +77,18 @@ class BookShelfSpec {
 
     @Test
     void shelfArrangedByBookTitle() {
-        shelf.add("Effective Java", "Code Complete", "The Mythical Man Month");
-        List<String> books = shelf.arrange();
-        List<String> expectedBooks = Arrays.asList("Code Complete", "Effective Java", "The Mythical Man Month");
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth);
+        List<Book> books = shelf.arrange();
+        List<Book> expectedBooks = Arrays.asList(codeComplete, effectiveJava, mythicalManMonth);
         assertEquals(expectedBooks, books, "Books should be sorted by title");
     }
 
     @Test
     void booksAreInInsertionOrderAfterArranging() {
-        shelf.add("Effective Java", "Code Complete", "The Mythical Man Month");
+        shelf.add(effectiveJava, codeComplete, mythicalManMonth);
         shelf.arrange();
-        List<String> books = shelf.books();
-        List<String> expectedBooks = Arrays.asList("Effective Java", "Code Complete", "The Mythical Man Month");
+        List<Book> books = shelf.books();
+        List<Book> expectedBooks = Arrays.asList(effectiveJava, codeComplete, mythicalManMonth);
         assertEquals(expectedBooks, books, "Books should be left in insertion order.");
     }
 
