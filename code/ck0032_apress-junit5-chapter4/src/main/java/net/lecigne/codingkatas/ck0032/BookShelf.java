f834f27e110f242149b51346aaca9d5fc681d4a9
Anthony Le Cigne <dev@lecigne.net>
2021-02-27 18:20:46 +0100

    [CK0032] Implement search by title with filter

--- code/ck0032_apress-junit5-chapter4/src/main/java/net/lecigne/codingkatas/ck0032/BookShelf.java
+++ code/ck0032_apress-junit5-chapter4/src/main/java/net/lecigne/codingkatas/ck0032/BookShelf.java
@@ -8,6 +8,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class BookShelf {
@@ -46,5 +47,16 @@ public class BookShelf {
         int percentageInProgress = booksInProgress * 100 / books.size();
         int percentageToRead = booksToRead * 100 / books.size();
         return new Progress(percentageToRead, percentageInProgress, percentageCompleted);
+    }
+
+    public List<Book> findBooksByTitle(String title) {
+        return findBooksByTitle(title, book -> true);
+    }
+
+    public List<Book> findBooksByTitle(String title, Predicate<Book> predicate) {
+        return books.stream()
+                .filter(book -> book.getTitle().toLowerCase().contains(title.toLowerCase()))
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 }
---
+++ code/ck0032_apress-junit5-chapter4/src/test/java/net/lecigne/codingkatas/ck0032/BookShelfSearchSpec.java
@@ -0,0 +1,43 @@
+package net.lecigne.codingkatas.ck0032;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import static java.time.Month.AUGUST;
+import static org.assertj.core.api.BDDAssertions.then;
+
+@DisplayName("Bookshelf search")
+@ExtendWith(BooksParameterResolver.class)
+class BookShelfSearchSpec {
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init(Map<String, Book> books) {
+        shelf = new BookShelf();
+        shelf.add(books.values().toArray(new Book[0]));
+    }
+
+    @Test
+    @DisplayName("when searching by title should return the correct books")
+    void bookshelfSearch_whenSearchingByTitle_shouldReturnCorrectBooks() {
+        List<Book> actualBooks = shelf.findBooksByTitle("code");
+        then(actualBooks).as("Books should contain titles containing \"code\"")
+                .hasSize(3);
+    }
+
+    @Test
+    @DisplayName("when searching by title and hint should return the correct books")
+    void bookshelfSearch_whenSearchingByTitleAndHint_shouldReturnCorrectBooks() {
+        Predicate<Book> predicate = book -> book.getPublishedOn().isBefore(LocalDate.of(2004, AUGUST, 1));
+        List<Book> actualBooks = shelf.findBooksByTitle("code", predicate);
+        then(actualBooks).as("Books should contain titles containing \"code\"")
+                .hasSize(2);
+    }
+}
