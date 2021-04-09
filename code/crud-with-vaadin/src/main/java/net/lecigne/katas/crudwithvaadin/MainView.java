e0ec525525ed44e998500098ac01c0be176dafa3
Anthony Le Cigne <dev@lecigne.net>
2021-04-09 19:18:13 +0200

    [CK0033] List entities in a data grid

--- code/crud-with-vaadin/src/main/java/net/lecigne/katas/crudwithvaadin/MainView.java
+++ code/crud-with-vaadin/src/main/java/net/lecigne/katas/crudwithvaadin/MainView.java
@@ -1,14 +1,22 @@
 package net.lecigne.katas.crudwithvaadin;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route
 public class MainView extends VerticalLayout {
+    private final CustomerRepository repo;
+    final Grid<Customer> grid;
 
-    public MainView() {
-        add(new Button("Click me", e -> Notification.show("Hello, Spring+Vaadin user!")));
+    public MainView(CustomerRepository repo) {
+        this.repo = repo;
+        this.grid = new Grid<>(Customer.class);
+        add(grid);
+        listCustomers();
+    }
+
+    private void listCustomers() {
+        grid.setItems(repo.findAll());
     }
 }
