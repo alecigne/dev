8bb83884083a01e34316ffe01b03986315f70eb1
Anthony Le Cigne <dev@lecigne.net>
2021-04-09 19:28:28 +0200

    [CK0033] Filter the data

--- code/crud-with-vaadin/src/main/java/net/lecigne/katas/crudwithvaadin/MainView.java
+++ code/crud-with-vaadin/src/main/java/net/lecigne/katas/crudwithvaadin/MainView.java
@@ -2,7 +2,10 @@ package net.lecigne.katas.crudwithvaadin;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
 
 @Route
 public class MainView extends VerticalLayout {
@@ -12,11 +15,19 @@ public class MainView extends VerticalLayout {
     public MainView(CustomerRepository repo) {
         this.repo = repo;
         this.grid = new Grid<>(Customer.class);
-        add(grid);
-        listCustomers();
+        TextField filter = new TextField();
+        filter.setPlaceholder("Filter by last name");
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+        add(filter, grid);
+        listCustomers("");
     }
 
-    private void listCustomers() {
-        grid.setItems(repo.findAll());
+    private void listCustomers(String filterText) {
+        if (StringUtils.isEmpty(filterText)) {
+            grid.setItems(repo.findAll());
+        } else {
+            grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
+        }
     }
 }
